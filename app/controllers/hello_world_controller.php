25bb527602bd80d19697e154174f60ecd801f880
Riku Kalinen <riku@kalinen.net>
2017-03-14 09:09:06 +0200

    dokumentoitu ongelma, säädetty kontrolleria

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,14 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController{
 
     public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+        // Testaa koodiasi täällä
+        echo 'Hello World!';
     }
-  }
+}
