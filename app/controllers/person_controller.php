5edb58e5e8e9ac1968baf16c730c045293a5a516
Riku Kalinen <riku@kalinen.net>
2017-03-27 21:59:42 +0300

    show

--- app/controllers/person_controller.php
+++ app/controllers/person_controller.php
@@ -6,6 +6,11 @@ class PersonController extends BaseController {
         $persons = Person::all();
         View::make('person/index.html', array('persons' => $persons));
     }
+
+    public static function show($id) {
+        $person = Person::find($id);
+        View::make('person/show.html', array('person' => $person));
+    }
 }
 
 ?>
