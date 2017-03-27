1d2c52fe696530b311a756ce5006a8dc622b13e8
Riku Kalinen <riku@kalinen.net>
2017-03-27 12:12:33 +0300

    person model

---
+++ app/models/person.php
@@ -0,0 +1,53 @@
+<?php
+
+class Person extends BaseModel {
+    public $id, $name, $username, $email, $password, $admin;
+
+    public function __construct($attributes){
+        parent::__construct($attributes);
+    }
+
+    public static function all() {
+        $query = DB::connection()->prepare("SELECT * FROM person");
+        $query->execute();
+        $rows = $query->fetchAll();
+        $persons = array();
+
+        foreach ($rows as $row) {
+            $persons[]
+                = new Person(array(
+                    'id' => $row['id'],
+                    'name' => $row['name'],
+                    'username' => $row['username'],
+                    'email' => $row['email'],
+                    'password' => $row['password'],
+                    'admin' => $row['admin']));
+        }
+        
+        return $persons;
+        
+    } # all
+
+    public static function find($id) {
+        $query = DB::connection()->prepare("SELECT * FROM person WHERE id = $id LIMIT 1");
+        $query->execute();
+        $row = $query->fetch();
+
+        if ($row) {
+            $person
+                = new Person(array(
+                    'id' => $row['id'],
+                    'name' => $row['name'],
+                    'username' => $row['username'],
+                    'email' => $row['email'],
+                    'password' => $row['password'],
+                    'admin' => $row['admin']));
+            return $person;
+        }
+
+        return null;
+
+    } # find
+}
+
+?>
