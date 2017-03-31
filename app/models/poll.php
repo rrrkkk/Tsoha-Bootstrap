62c6e1d58b0365bf68e2aec0a568e1f6effe6c2d
Riku Kalinen <riku@kalinen.net>
2017-03-31 08:29:26 +0300

    replacing stupid loops using sql

--- app/models/poll.php
+++ app/models/poll.php
@@ -2,13 +2,20 @@
 
 class Poll extends BaseModel {
     public $id, $person_id, $name, $startdate, $enddate, $anonymous, $poll_type_id;
+    public $person_name, $poll_type_name; // these are derived from elsewhere in the db.
 
     public function __construct($attributes){
         parent::__construct($attributes);
     }
 
     public static function all() {
-        $query = DB::connection()->prepare("SELECT * FROM poll");
+        $sql = "SELECT poll.id AS id, person_id, poll.name AS name,
+                       startdate, enddate, anonymous, poll_type_id,
+                       person.name AS person_name, poll_type.name AS poll_type_name
+                FROM poll
+                INNER JOIN person ON person.id = person_id
+                INNER JOIN poll_type ON poll_type.id = poll_type_id";
+        $query = DB::connection()->prepare($sql);
         $query->execute();
         $rows = $query->fetchAll();
         $polls = array();
@@ -22,7 +29,9 @@ class Poll extends BaseModel {
                     'startdate' => $row['startdate'],
                     'enddate' => $row['enddate'],
                     'anonymous' => $row['anonymous'],
-                    'poll_type_id' => $row['poll_type_id']));
+                    'poll_type_id' => $row['poll_type_id'],
+                    'person_name' => $row['person_name'],
+                    'poll_type_name' => $row['poll_type_name']));
         }
         
         return $polls;
@@ -30,7 +39,14 @@ class Poll extends BaseModel {
     } # all
 
     public static function find($id) {
-        $query = DB::connection()->prepare("SELECT * FROM poll WHERE id = :id LIMIT 1");
+        $sql = "SELECT poll.id AS id, person_id, poll.name AS name,
+                       startdate, enddate, anonymous, poll_type_id,
+                       person.name AS person_name, poll_type.name AS poll_type_name
+                FROM poll
+                INNER JOIN person ON person.id = person_id
+                INNER JOIN poll_type ON poll_type.id = poll_type_id
+                WHERE poll.id = :id LIMIT 1";
+        $query = DB::connection()->prepare($sql);
         $query->bindValue(':id', $id, PDO::PARAM_INT);
         $query->execute();
         $row = $query->fetch();
@@ -44,7 +60,9 @@ class Poll extends BaseModel {
                     'startdate' => $row['startdate'],
                     'enddate' => $row['enddate'],
                     'anonymous' => $row['anonymous'],
-                    'poll_type_id' => $row['poll_type_id']));
+                    'poll_type_id' => $row['poll_type_id'],
+                    'person_name' => $row['person_name'],
+                    'poll_type_name' => $row['poll_type_name']));
             return $poll;
         }
 
